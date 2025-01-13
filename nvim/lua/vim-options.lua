13ec54ef59a561cdf94fc66e23b03133edaf6644
hoainam1401 <ngohoainam1401@gmail.com>
2025-01-13 14:57:19 +0000

    new config

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -16,4 +16,21 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+
 vim.opt.colorcolumn = "94"
+
+vim.keymap.set("n", "<leader>ce", function()
+    local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+    if #diagnostics > 0 then
+        local message = diagnostics[1].message
+        vim.fn.setreg("+", message)
+        print("Copied diagnostic: " .. message)
+    else
+        print("No diagnostic at cursor")
+    end
+end, { noremap = true, silent = true })
+
+-- go to errors in a file :/
+vim.keymap.set("n", "<leader>ne", vim.diagnostic.goto_next) -- next err
+vim.keymap.set("n", "<leader>pe", vim.diagnostic.goto_prev) -- previous err
