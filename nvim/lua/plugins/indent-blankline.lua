c29487a5b03f7eeda757d5f3e0f5165d20aad0c3
hoainam1401 <ngohoainam1401@gmail.com>
2025-02-01 12:58:09 +0000

    fix

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,8 +1,18 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	main = "ibl",
-	opts = {
-		indent = { char = "┊" },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		main = "ibl",
+		opts = {
+			indent = { char = "┊" },
+		},
+	},
+	{
+		"lukas-reineke/virt-column.nvim",
+		opts = {
+			char = "│",
+			virtcolumn = "94",
+			hihlight = "WarningMsg",
+		},
 	},
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "clangd", "jdtls" },
+				ensure_installed = { "lua_ls", "pyright", "clangd", "jdtls", "ts_ls" },
 			})
 		end,
 	},
@@ -32,6 +32,9 @@ return {
 			lspconfig.jdtls.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
