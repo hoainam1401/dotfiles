50c6032108e29b7a94e57fea5b6e2d4a696fe67e
hoainam1401 <ngohoainam1401@gmail.com>
2025-02-08 18:43:44 +0000

    new nvim config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -32,9 +32,9 @@ return {
 			lspconfig.jdtls.setup({
 				capabilities = capabilities,
 			})
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -21,14 +21,14 @@ vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.opt.colorcolumn = "94"
 
 vim.keymap.set("n", "<leader>ce", function()
-  local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
-  if #diagnostics > 0 then
-    local message = diagnostics[1].message
-    vim.fn.setreg("+", message)
-    print("Copied diagnostic: " .. message)
-  else
-    print("No diagnostic at cursor")
-  end
+	local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+	if #diagnostics > 0 then
+		local message = diagnostics[1].message
+		vim.fn.setreg("+", message)
+		print("Copied diagnostic: " .. message)
+	else
+		print("No diagnostic at cursor")
+	end
 end, { noremap = true, silent = true })
 
 -- go to errors in a file :/
