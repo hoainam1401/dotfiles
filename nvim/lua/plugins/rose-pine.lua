ff2d7b91799bf00979e7f90430c333aef66fd405
hoainam1401 <ngohoainam1401@gmail.com>
2025-02-15 15:31:31 +0000

    Create rose-pine.lua

---
+++ nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,25 @@
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("rose-pine").setup({
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
+
+			enable = {
+				terminal = true,
+				legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+				migrations = true, -- Handle deprecated options automatically
+			},
+
+			styles = {
+				bold = true,
+				italic = true,
+				transparency = true,
+			},
+		})
+		vim.cmd.colorscheme("rose-pine-moon")
+	end,
+}
