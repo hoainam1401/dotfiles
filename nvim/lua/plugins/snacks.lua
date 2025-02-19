c0b7e76392755f1184f775bb6f3b09443a3c928d
hoainam1401 <ngohoainam1401@gmail.com>
2025-02-19 11:18:17 +0000

    fix

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/snacks.nvim",
+  priority = 1000,
+	---@type snacks.Config
+	opts = {
+		image = {
+			-- your image configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+}
