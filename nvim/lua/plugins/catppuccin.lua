4cffa4429ba0084eb341ff6c100c4fd96b443019
hoainam1401 <ngohoainam1401@gmail.com>
2025-02-22 12:00:26 +0000

    Update catppuccin.lua

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,29 +1,35 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
-			native_lsp = {
-				enabled = true,
-				virtual_text = {
-					errors = "italic",
-					hints = "italic",
-					warnings = "italic",
-					information = "italic",
-					ok = "italic",
-				},
-				underlines = {
-					errors = "underline",
-					hints = "underline",
-					warnings = "underline",
-					information = "underline",
-					ok = "underline",
-				},
-			},
-		})
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = "italic",
+          hints = "italic",
+          warnings = "italic",
+          information = "italic",
+          ok = "italic",
+        },
+        underlines = {
+          errors = "underline",
+          hints = "underline",
+          warnings = "underline",
+          information = "underline",
+          ok = "underline",
+        },
+        intergrations = {
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = true,
+          },
+        },
+      },
+    })
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end,
 }
