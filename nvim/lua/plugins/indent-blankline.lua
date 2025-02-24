8800c0573bd262546f8c945b64ffba25687e921d
hoainam1401 <ngohoainam1401@gmail.com>
2025-02-24 20:42:18 +0000

    Update indent-blankline.lua

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -12,23 +12,23 @@ return {
 		config = function()
 			local indent_blankline = vim.api.nvim_create_augroup("inndent_blankline_augroup", { clear = true })
 			local highlight = {
+        "RainbowGreen",
+        "RainbowViolet",
 				"RainbowRed",
 				"RainbowYellow",
 				"RainbowBlue",
 				"RainbowOrange",
-				"RainbowGreen",
-				"RainbowViolet",
 				"RainbowCyan",
 			}
 			local hooks = require("ibl.hooks")
 
 			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#A6E3A1" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#CBA6F7" })
 				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#F38BA8" })
 				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#F9E2AF" })
 				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#89B4FA" })
 				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#FAB387" })
-				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#A6E3A1" })
-				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#CBA6F7" })
 				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#89DCEB" })
 			end)
 			vim.g.rainbow_delimiters = { highlight = highlight }
