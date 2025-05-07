e7a093b4420550804863cc49d1de0ad5ef9c89e5
hoainam1401 <ngohoainam1401@gmail.com>
2025-05-07 14:30:04 +0100

    update oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,8 +1,26 @@
 return {
 	"stevearc/oil.nvim",
+	dependencies = { "echasnovski/mini.icons" },
 	config = function()
 		local oil = require("oil")
-		oil.setup()
+		oil.setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = false,
+			columns = { "icon" },
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					return name == ".." or name == ".git"
+				end,
+			},
+			win_options = {
+				wrap = true,
+				cursorcolumn = false,
+			},
+			watch_for_changes = true,
+			use_default_keymaps = true,
+		})
 		vim.keymap.set("n", "-", oil.toggle_float, { desc = "Toggle float oil" })
 	end,
 }
