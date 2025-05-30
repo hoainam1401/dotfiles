510f89760526d819b643a6bd60c4bb506a7d190f
hoainam1401 <ngohoainam1401@gmail.com>
2025-05-31 00:56:03 +0100

    Update alpha.lua

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -23,12 +23,12 @@ return {
 			os.date("\t\t\t\t\t\t Today is %d(%A) of %B"),
 		}
 		dashboard.section.buttons.val = {
-			dashboard.button("f", "📜 Find file", ":Telescope find_files <CR>"),
-			dashboard.button("e", "📝 New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("r", "📉 Recently used files", ":Telescope oldfiles <CR>"),
-			dashboard.button("t", "🔍 Find text", ":Telescope live_grep <CR>"),
-			dashboard.button("c", "🔥 Configuration", ":e ~/.config/nvim/init.vim<CR>"),
-			dashboard.button("q", "💣 Quit Neovim", ":qa<CR>"),
+			dashboard.button("f", "󰈞 Find file", ":Telescope find_files <CR>"),
+			dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("r", "󰷊 Recently used files", ":Telescope oldfiles <CR>"),
+			dashboard.button("t", "󱎸 Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("c", " Configuration", ":e ~/.config/nvim/init.vim<CR>"),
+			dashboard.button("q", "󰅙 Quit Neovim", ":qa<CR>"),
 		}
 
 		dashboard.opts.opts.noautocmd = true
