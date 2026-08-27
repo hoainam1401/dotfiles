33199971f1c9ed3807f0f367c8ea3bb084b41ebb
HoaiNam <ngohoainam1401@gmail.com>
2026-08-27 23:41:21 +0100

    update

--- sketchybar/items/cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item cpu right \
-           --set cpu  update_freq=2 \
-                      icon=􀧓 \
-                      label.color=$RED \
-                      icon.color=$RED   \
-                      script="$PLUGIN_DIR/cpu.sh"
--- sketchybar/items/volume.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item volume right \
-           --set volume script="$PLUGIN_DIR/volume.sh" \
-                        label.color=$YELLOW \
-                        icon.color=$YELLOW   \
-           --subscribe volume volume_change 
